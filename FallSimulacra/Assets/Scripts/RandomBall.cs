9c3f51dc7083819f9b9afa808dd798badf9510cb
MSI\banba <banban.n.namchan@gmail.com>
2022-12-06 14:39:36 +0900

    データ修復　ボールのランダム落下　クリックでボールを飛ばす等

---
+++ FallSimulacra/Assets/Scripts/RandomBall.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBall : MonoBehaviour
+{
+    public GameObject[] falls;
+    public GameObject ball;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % 150 == 0)
+        {
+            GameObject fall = Instantiate(
+                falls[Random.Range(0, falls.Length)],
+                new Vector3(Random.Range(-14f, 14f), transform.position.y, transform.position.z),
+                Quaternion.identity
+                );
+            Destroy(fall, 5f);
+        }
+    }
+}
