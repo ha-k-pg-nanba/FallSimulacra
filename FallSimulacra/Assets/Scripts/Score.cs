761ed8493198fb069d01b17578585fecafebc0cb
MSI\banba <banban.n.namchan@gmail.com>
2023-01-31 12:00:08 +0900

    効果音　BGMの追加　ボールをカラフルにして　androidに移行してもバグはなし　完成

---
+++ FallSimulacra/Assets/Scripts/Score.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6fee7eb7e3f166249e3bb29eb7774f4ed366bfad278795ae712706ffe6cc828c
+size 513
---
+++ FallSimulacra/Assets/Scripts/Target_Black.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3fd4ab4729ead7a44ab26a560c506ce039a14496120fff107f41a89dc44f941
+size 592
